53e55143d8dfdfcfc1934cc267201ec0ea6e8936
Samuel Pacris II <samuelpacris5@gmail.com>
2026-03-29 23:26:51 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+ <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © <a href="https://itsourcecode.com" target="_blank">Itsourcecode.com</a></span>
+          </div>
+        </div>
+      </footer>
